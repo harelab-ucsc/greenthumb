3f4596f3968fbe1ea1885a572b50d45fdab69d16
David Glover <david05glover@gmail.com>
2026-05-01 17:41:23 -0700

    removed data from github

--- Sensing/teros12/scripts/teros12_stream.py
+++ Sensing/teros12/scripts/teros12_stream.py
@@ -27,20 +27,20 @@ from teros12 import Teros12
 sensor_config = [
     {
         "sensor_name" : "sensor_1",
-        "depth" : 3,
+        "depth" : 6,
         "depth_units" : "inch",
         "usb_serial": "aa192e3c9eb2ed11840d4daca7669f5d"
     }
     , 
     {
         "sensor_name" : "sensor_2",
-        "depth" : 6,
+        "depth" : 9,
         "depth_units" : "inch",
         "usb_serial": "14d0f0dda5b2ed118ecd4faca7669f5d"
     },
     {
         "sensor_name" : "sensor_3",
-        "depth" : 9,
+        "depth" : 12,
         "depth_units" : "inch",
         "usb_serial": "9e12fd528fb2ed119ea44eaca7669f5d"
     }
